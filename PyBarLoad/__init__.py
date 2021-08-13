1b64136a5498254bf764f7875edf799dbe315e2a
Katsuhi-Dev <maxencedu62_@outlook.fr>
2021-08-13 22:43:12 +0200

    V1.1 PyLoadBar

--- PyBarLoad/__init__.py
+++ PyBarLoad/__init__.py
@@ -1,22 +1,67 @@
 """
 By Prasax And Katsu'hi !
 
-My github : https://www.github.com/Prasax-Dev
+Our github : https://www.github.com/Prasax-Dev
+            https://www.github.com/Kijusu-Dev
 """
 
 import time
 
 class PyBar:
     def Bar(finish:int = None, end_text:str = None):
-        i = 0
-        count = 0
         finish = finish
         for i in range(finish):
             i += 1
-            count += 1
-            text = print("■" * i, f'{count} %')
+            print(f"\r{'#'*i}{'-'*(finish-i)}\t||➡ {i}% ||\t", end='')
             time.sleep(1)
         if not end_text:
             print("Finished bar !")
         else:
             print(end_text)
+
+    def LoadingBar(finish:int = None, end_text:str = None):
+        finish = finish
+        for i in range(finish):
+            i += 1
+            print(f"\r{'▆' * i}{'-' * (finish - i)}\t||➡ {i}% ||\t", end='')
+            time.sleep(1)
+        if not end_text:
+            print("Finished bar !")
+        else:
+            print(end_text)
+
+    def StarBar(finish:int = None, end_text:str = None):
+        finish = finish
+        for i in range(finish):
+            i += 1
+            print(f"\r{'*' * i}{'-' * (finish - i)}\t||➡ {i}% ||\t", end='')
+            time.sleep(1)
+        if not end_text:
+            print("Finished bar !")
+        else:
+            print(end_text)
+
+    def PersonalizeBar(finish:int = None, symbol:str = None, end_text:str = None):
+        finish = finish
+        symbol = symbol
+        for i in range(finish):
+            i += 1
+            print(f"\r{f'{symbol}' * i}{'-' * (finish - i)}\t||➡ {i}% ||\t", end='')
+            time.sleep(1)
+        if not end_text:
+            print("Finished bar !")
+        else:
+            print(end_text)
+
+    def colors(color: str = None, text: str = None):
+        ESCAPE = "\x1b"
+        colors = {
+            "BLACK": "[30m",
+            "RED": "[31m",
+            "GREEN": "[32m",
+            "YELLOW": "[33m",
+            "BLUE": "[34m",
+            "PURPLE": "[35m",
+            "CYAN": "[36m"
+        }
+        print(f"{ESCAPE + colors[color] + text}")
